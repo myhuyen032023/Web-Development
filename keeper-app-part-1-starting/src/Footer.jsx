25f8b38581c18a79bbc6161cb6b2dc7a0b5cef6c
My Huyen <myhuyen032023@gmail.com>
2023-08-26 09:48:09 +0700

    First Commit

---
+++ keeper-app-part-1-starting/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Footer() 
+{
+    return <footer>
+        <p>Copyright ⓒ {new Date().getFullYear()}</p>
+    </footer>;
+}
+
+export default Footer;
